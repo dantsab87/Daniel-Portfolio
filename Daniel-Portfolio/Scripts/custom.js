262625925e4ad71559db03b6f3f6e846f024e87c
Daniel Chang <dantsab87@gmail.com>
2019-11-25 12:42:24 -0500

    email not working

---
+++ Daniel-Portfolio/Scripts/custom.js
@@ -0,0 +1,11 @@
+﻿$("#emailclick").click(function () {
+    Email.send({
+        SecureToken: "b19966dc-f12e-464f-ad94-80e6f5d42658",
+        To: 'dantsab87@gmail.com',
+        From: $("#email").val(),
+        Subject: $("#subject").val(),
+        Body: "From " + $("#name").val() + ": " + $("#msg").val()
+    }).then(
+        message => alert("Email has been sent!")
+    );
+});
