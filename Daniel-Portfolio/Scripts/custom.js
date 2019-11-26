99e08089793ade7ca5faf4a84031d535f30728f5
Daniel Chang <dantsab87@gmail.com>
2019-11-26 10:50:00 -0500

    sweet alerts

--- Daniel-Portfolio/Scripts/custom.js
+++ Daniel-Portfolio/Scripts/custom.js
@@ -1,11 +1,19 @@
-﻿$("#emailclick").click(function () {
-    Email.send({
-        SecureToken: "b19966dc-f12e-464f-ad94-80e6f5d42658",
-        To: 'dantsab87@gmail.com',
-        From: $("#email").val(),
-        Subject: $("#subject").val(),
-        Body: "From " + $("#name").val() + ": " + $("#msg").val()
-    }).then(
-        message => alert("Email has been sent!")
-    );
-});
+﻿
+
+    $("#emailclick").click(function () {
+        if ($("#email").val() && $("#subject").val() && $("#msg").val() && $("#name").val() != null) {
+
+            Email.send({
+                SecureToken: "384fcf19-3202-46d3-ad60-19643ed4f02d",
+                To: 'dantsab87@gmail.com',
+                From: $("#email").val(),
+                Subject: $("#subject").val(),
+                Body: "From " + $("#name").val() + ": " + $("#msg").val()
+            }).then(
+                swal("Email Sent!", "", "success")
+            );
+        }
+        else {
+            swal("Woops!", "Please fill in the form!", "error");
+        }
+    });
